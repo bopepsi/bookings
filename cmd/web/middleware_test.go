5779ea2aa82b719a05b2b9b598fae82fefc136a0
bopepsi <bopepsi@gmail.com>
2022-09-18 17:06:28 -0400

    middleware-test

--- cmd/web/middleware_test.go
+++ cmd/web/middleware_test.go
@@ -14,7 +14,21 @@ func TestNosurf(t *testing.T) {
 	case http.Handler:
 		// do nothing
 	default:
-		t.Error("type is not heep handler")
+		t.Error("type is not http handler")
+	}
+
+}
+
+func TestSessionLoad(t *testing.T) {
+
+	var myH myHandler
+	h := SessionLoad(myH)
+
+	switch h.(type) {
+	case http.Handler:
+		// do nothing
+	default:
+		t.Error("type is not http Handler")
 	}
 
 }
