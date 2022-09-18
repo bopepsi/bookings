94540c95509221b3071eb43b63938b83366def60
bopepsi <bopepsi@gmail.com>
2022-09-18 11:59:48 -0400

    add-nosurf-test

---
+++ cmd/web/middleware_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestNosurf(t *testing.T) {
+
+	var myH myHandler
+	h := NoSurf(myH)
+
+	switch h.(type) {
+	case http.Handler:
+		// do nothing
+	default:
+		t.Error("type is not heep handler")
+	}
+
+}
